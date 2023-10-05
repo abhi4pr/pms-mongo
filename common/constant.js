b838082bbe3c18541f234cd00fbe85769f3fc0a7
Sumit <sumit223312@gmail.com>
2023-10-05 18:10:03 +0530

    Adding Object mast and make api for counts instac and instap models and changes fields in register camapign and also changes in register campaign content section module

--- common/constant.js
+++ common/constant.js
@@ -1,5 +1,7 @@
 module.exports = Object.freeze({
   base_url: "http://34.93.135.33:8080",
-  CREATOR_INSIGHTS :"https://app.ylytic.com/ylytic/api/v1/rt_tracking/insights",
-  CF_INSTA_API :"https://www.instagram.com/trolls_official/?__a=1&__d=dis"
+  CREATOR_INSIGHTS: "https://app.ylytic.com/ylytic/api/v1/rt_tracking/insights",
+  CF_INSTA_API: "https://www.instagram.com/trolls_official/?__a=1&__d=dis",
+  INSTA_C_MODELS: 1,
+  INSTA_P_MODELS: 2,
 });
---
+++ controllers/objmast.js
@@ -0,0 +1,102 @@
+const objectMastSchema = require("../models/objmast.js");
+exports.addObjectMast = async (req, res) => {
+  try {
+    const { obj_name, soft_name, dept_id, created_by } = req.body;
+
+    const Obj = new objectMastSchema({
+      obj_name,
+      soft_name,
+      Dept_id: dept_id,
+      Created_by: created_by,
+    });
+
+    const savedObjectMast = await Obj.save();
+    res.send({
+      data: savedObjectMast,
+      status: 200,
+      message: "Object and user_auth_detail added successfully",
+    });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .send({ error: err, message: "This objMast cannot be created" });
+  }
+};
+
+exports.getObjectMastById = async (req, res) => {
+  try {
+    let match_condition = {
+      obj_id: parseInt(req.params.id),
+    };
+
+    let objects = await objectMastSchema.aggregate([
+      {
+        $match: match_condition,
+      },
+      {
+        $lookup: {
+          from: "departmentmodels",
+          localField: "Dept_id",
+          foreignField: "dept_id",
+          as: "data",
+        },
+      },
+
+      {
+        $unwind: "$data",
+      },
+      {
+        $addFields: {
+          dept_name: "$data.dept_name",
+        },
+      },
+      {
+        $project: {
+          data: 0,
+        },
+      },
+    ]);
+
+    if (objects.length === 0) {
+      res
+        .status(200)
+        .send({ success: true, data: [], message: "No Record found" });
+    } else {
+      return res.status(200).send({ success: true, data: objects[0] });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ error: err, message: "Error getting  Object" });
+  }
+};
+
+exports.getObjectMasts = async (req, res) => {
+  try {
+    let objets = await objectMastSchema.aggregate([
+      {
+        $lookup: {
+          from: "departmentmodels",
+          localField: "Dept_id",
+          foreignField: "dept_id",
+          as: "data",
+        },
+      },
+
+      {
+        $unwind: "$data",
+      },
+    ]);
+    if (objets.length === 0) {
+      res
+        .status(200)
+        .send({ success: true, data: [], message: "No Record found" });
+    } else {
+      return res.status(200).send({ success: true, data: objets });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ error: err, message: "Error getting all Objects" });
+  }
+};
+
