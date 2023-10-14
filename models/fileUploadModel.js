9dd4e5e9d688c2989e78776e3aba4df46a3f5fb1
Sumit <sumit223312@gmail.com>
2023-10-14 12:13:31 +0530

    Changes in product module and also some modification for multiple file uploads

---
+++ models/fileUploadModel.js
@@ -0,0 +1,27 @@
+const { default: mongoose } = require("mongoose");
+const AutoIncrement = require("mongoose-auto-increment");
+
+const fileUploadSchema = new mongoose.Schema({
+  fileId: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  contentSecRegId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "contentSectionRegCmpModel",
+    required:true
+  },
+  contentSecFile: {
+    type: String,
+  },
+});
+
+AutoIncrement.initialize(mongoose.connection);
+fileUploadSchema.plugin(AutoIncrement.plugin, {
+  model: "fileUploadModel",
+  field: "fileId",
+  startAt: 1,
+  incrementBy: 1,
+});
+module.exports = mongoose.model("fileUploadModel", fileUploadSchema);
