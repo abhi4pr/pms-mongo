d0f5e69936653b21b852c21ddd93edcc22e9652c
abhi4pr <ascs739@gmail.com>
2023-10-31 17:51:12 +0530

    new mongo connect

--- variables.js
+++ variables.js
@@ -3,5 +3,5 @@ module.exports = {
     API_URL: 'http://0.0.0.0:8080/',
     JWT: 'secret',
     MONGODB: 'mongodb+srv://root:MongoDBPassword123@atlascluster.dhgrrgb.mongodb.net/instaDB?retryWrites=true&w=majority',
-    // MONGODB: 'mongodb://35.200.234.121:27017/'
+    // MONGODB: 'mongodb+srv://tushar:tusharDBUserPassword@cluster0.s9g7bli.mongodb.net/instaDB?retryWrites=true&w=majority'
 };
