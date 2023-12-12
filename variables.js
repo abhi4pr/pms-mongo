f181acfefe6f6d2b11d22f56f5232324ef0f6761
Sumit Sen <sumit223312@gmail.com>
2023-12-12 18:30:27 +0530

    Create api for phase dashboard in operation module

--- variables.js
+++ variables.js
@@ -2,8 +2,8 @@ module.exports = {
     PORT: 8080,
     API_URL: 'http://0.0.0.0:8080/',
     JWT: 'secret',
-    MONGODB: 'mongodb+srv://root:MongoDBPassword123@atlascluster.dhgrrgb.mongodb.net/instaDB?retryWrites=true&w=majority',
+    // MONGODB: 'mongodb+srv://root:MongoDBPassword123@atlascluster.dhgrrgb.mongodb.net/instaDB?retryWrites=true&w=majority',
     // MONGODB :'mongodb+srv://manojgavel1:Manojatlas123@cluster0.6m1smop.mongodb.net/instaDB?retryWrites=true&w=majority'
-    // MONGODB: 'mongodb://0.0.0.0:27017',
+    MONGODB: 'mongodb://0.0.0.0:27017',
     // MONGODB: 'mongodb+srv://tushar:tusharDBUserPassword@cluster0.s9g7bli.mongodb.net/instaDB?retryWrites=true&w=majority'
 };
