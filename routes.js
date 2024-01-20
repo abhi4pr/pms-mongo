f9848e0e50d9821a3b8dc44afc73c349effdff50
abhi4pr <ascs739@gmail.com>
2024-01-20 13:55:27 +0530

    php finance changes, add asset tag person api

--- routes.js
+++ routes.js
@@ -107,6 +107,7 @@ const task = require("./controllers/TMS/task.js")
 const taskSequence = require("./controllers/TMS/taskSequence.js");
 const assetReturnRequest = require("./controllers/assetReturn.js");
 const newCoc = require('./controllers/cocManagementNew.js')
+const phpVendorPaymentRequest = require("./controllers/phpVendorPaymentRequest.js");
 
 /* Task Mangement Controller Import */
 const deptWiseStatus = require('./controllers/TMS/deptWiseStatus.js');
@@ -1042,6 +1043,7 @@ router.get("/get_total_asset_in_category/:category_id", sim.getTotalAssetInCateg
 router.get("/get_total_asset_in_category_allocated/:category_id", sim.getTotalAssetInCategoryAllocated);
 router.get("/show_asset_hr_data", sim.showAssetDataToHR);
 router.get("/show_asset_user_data/:user_id", sim.showAssetDataToUser);
+router.get("/show_new_asset_user_data/:user_id", sim.showNewAssetDataToUser);
 
 /* Asset Category Routes */
 router.post("/add_asset_category", assetCategory.addAssetCategory);
@@ -1366,6 +1368,7 @@ router.put("/edit_data_new", dataController.editDataNew);
 router.get("/distinct_created_by", dataController.DistinctCreatedByWithUserName);
 router.get("/distinct_designed_by", dataController.DistinctDesignedByWithUserName);
 router.get("/images_with_data_name/:data_name", dataController.ImagesWithDataName);
+router.get('/total_count_data', dataController.totalCountOfData);
 
 //---------------------------------------------------------------------------All Routes OF Data Module Ends Here ---------------------------------------------------------------------------------------------------//
 
@@ -1396,5 +1399,11 @@ router.post("/task_sequence", taskSequence.addSequence)
 router.get("/task_sequence", taskSequence.getSequences)
 router.put("/task_sequence", taskSequence.editTaskSequence)
 router.delete("/task_sequence/:_id", taskSequence.deleteTaskSequence)
+
+//phpVendorRequest Routes
+router.post("/phpvendorpaymentrequest", upload1.single('evidence'), phpVendorPaymentRequest.addPhpVendorPaymentRequest);
+router.get("/phpvendorpaymentrequest", phpVendorPaymentRequest.getPhpVendorPaymentRequests);
+router.get("/phpvendorpaymentrequest/:request_id", phpVendorPaymentRequest.getSinglePhpVendorPaymentRequest)
+router.put("/phpvendorpaymentrequest", upload1.single('evidence'), phpVendorPaymentRequest.updatePhpVendorPaymentRequest)
 
 module.exports = router;
