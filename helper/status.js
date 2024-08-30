7bfffaa093387bd4714224929d403ab483551e78
Vijayant <vijayanttrivedi1500@gmail.com>
2024-08-30 15:19:10 +0530

    Aman Changes

--- helper/status.js
+++ helper/status.js
@@ -56,5 +56,5 @@ module.exports = {
     incentiveCalculationUserLimit: 50000,
     //email for Sales booking and payment update
     // salesEmail: "amanrathod197@gmail.com,naveen@creativefuel.io"
-    salesEmail: "nikhil@creativefuel.io,tushar@creativefuel.io,tiya@creativefuel.io"
+    salesEmail: "nikhil@creativefuel.io,tushar@creativefuel.io,tiyawadhwani@creativefuel.io"
 }
